8a9242640e270140f61d37e5f9f60c92795b8ad4
oscarfuentes22 <97373140+oscarfuentes22@users.noreply.github.com>
2023-07-24 13:01:18 -0600

    perfil sin metodo terminado, Hobby

--- Plataformas moviles/Laboratorios/lab2/src/main/kotlin/PerfilUsuario.kt
+++ Plataformas moviles/Laboratorios/lab2/src/main/kotlin/PerfilUsuario.kt
@@ -1,10 +1,7 @@
-class PerfilUsuario(var ID: Int, var nombre: String, var apellido: String, var UrlPhoto: String, var Edad: Int, var Correo: String, var Biografia: String, val Estado: List<String>, var Hobbies: List<Hobby>) {
+class PerfilUsuario(var ID: Int, var nombre: String, var apellido: String, var UrlPhoto: String, var Edad: Int, var Correo: String, var Biografia: String, val Estado: List<String>, var Hobbies: MutableList<Hobby>) {
 
-    val Estado = listOf("Activo", "Pendiente", "Inactivo")
-    var Hobbies = mutableListOf<Hobby>()
     fun AgregarHobby(Hobby: Hobby) {
-        Titulo = "Movie"
-        Hobby = (Titulo, Descripcion, UrlPhoto)
+
         Hobbies.add(Hobby)
     }
 }
