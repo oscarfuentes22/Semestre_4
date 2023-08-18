0adf391c5e2ae46807e2d64251d6f94c8364fc33
oscarfuentes22 <97373140+oscarfuentes22@users.noreply.github.com>
2023-08-17 23:56:36 -0600

    pantalla2 finish

--- Plataformas moviles/Laboratorios/lab4/app/src/main/java/com/oscar/lab4/MainActivit2y.kt
+++ Plataformas moviles/Laboratorios/lab4/app/src/main/java/com/oscar/lab4/MainActivit2y.kt
@@ -1,28 +1,36 @@
 package com.oscar.lab4
 
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -45,7 +53,10 @@ class MainActivity2 : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Pantalla2( R.drawable.outline_add_home_24 , R.drawable.imagen_5)
+                    Pantalla2( R.drawable.outline_add_home_24,
+                        R.drawable.imagen_5,
+                        R.drawable.perfil,
+                    )
                 }
             }
         }
@@ -53,9 +64,16 @@ class MainActivity2 : ComponentActivity() {
 }
 
 @Composable
-fun Pantalla2(@DrawableRes id5 : Int, @DrawableRes id6 : Int) {
+fun Pantalla2(@DrawableRes id5 : Int,
+              @DrawableRes id6 : Int,
+              @DrawableRes id7 : Int,
+
+              ) {
     val outline_add_home_24 = painterResource(id = id5)
     val image5 = painterResource(id = id6)
+    val image6 = painterResource(id = id7)
+
+
 
     Column() {
         Row(modifier = Modifier.fillMaxWidth()) {
@@ -82,46 +100,153 @@ fun Pantalla2(@DrawableRes id5 : Int, @DrawableRes id6 : Int) {
             contentAlignment = Alignment.TopCenter,
             modifier = Modifier
                 .fillMaxWidth()
-                .height(300.dp)
+                .height(200.dp)
 
-        ){
+        ) {
             Image(
                 painter = image5,
                 contentDescription = "biblioteca",
-                contentScale = ContentScale.FillBounds
-            )
-            Text(
-                text = "My profile",
-                fontWeight = FontWeight.Bold,
-                fontStyle = FontStyle.Italic,
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .padding(5.dp),
-                textAlign = TextAlign.Center,
-                fontSize = 25.sp ,
-              
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .alpha(0.8f)
             )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .offset(y = 110.dp)
+                    .clip(RoundedCornerShape(75.dp))
+            ) {
+                Image(
+                    painter = image6,
+                    contentDescription = "perfil",
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .width(150.dp)
+                        .height(150.dp)
+                        .border(
+                            width = 2.dp,
+                            color = Color.Black,
+                            shape = RoundedCornerShape(75.dp)
+                        )
+                        .clip(RoundedCornerShape(75.dp))
+                        .align(Alignment.Center)
+                        .fillMaxSize()
+                        .background(Color.LightGray),
+                    contentScale = ContentScale.Crop
 
+                )
+            }
         }
 
-
-
-
+        Spacer(Modifier.height(70.dp))
+        Text(
+            text = "Oscar Andres Fuentes Dubón",
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            modifier = Modifier
+                .padding(5.dp),
+            textAlign = TextAlign.Center,
+            fontSize = 25.sp,
+            )
+        opcionDosTextos(
+            idImagen = R.drawable.icone_de_service_noir
+        )
+        opcionunTextos(
+            idImagen = R.drawable.perfil, texto = "My Friends", descrip = "amigos"
+        )
+        opcionunTextos(
+            idImagen = R.drawable.outline_calendar_month_24, texto = "Calendar", descrip = "calendario"
+        )
+        opcionunTextos(
+            idImagen = R.drawable.outline_book_24, texto = "My Couses" , descrip = "cursos"
+        )
+        opcionunTextos(
+            idImagen = R.drawable.baseline_grading_24, texto = "My Grades" , descrip = "notas"
+        )
+        opcionunTextos(
+            idImagen = R.drawable.baseline_fingerprint_24, texto = "My Groups" , descrip = "grupos"
+        )
+        opcionunTextos(
+            idImagen = R.drawable.outline_event_24, texto = "My Upcoming Events" , descrip = "eventos"
+        )
     }
 }
+@Composable
+fun opcionDosTextos(@DrawableRes idImagen : Int){
+    Spacer(modifier = Modifier
+        .fillMaxWidth()
+        .height(1.dp)
+        .background(Color.LightGray)
+    )
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Image(
+            painter = painterResource(id = idImagen),
+            contentDescription = "perfil",
+            modifier = Modifier
+                .width(50.dp)
+                .height(50.dp)
+                .padding(5.dp)
+        )
+        Column {
+            Text(
+                text = "My campus",
+                color = Color.Gray
+            )
 
+            Text(
+                text = "Campus Central")
+        }
+    }
+}
+@Composable
+fun opcionunTextos(@DrawableRes idImagen : Int, texto :  String, descrip : String) {
+    Spacer(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+            .background(Color.LightGray)
+    )
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Image(
+            painter = painterResource(id = idImagen),
+            contentDescription = descrip,
+            modifier = Modifier
+                .width(50.dp)
+                .height(50.dp)
+                .padding(5.dp)
+        )
+        Column {
+            Text(
+                text = texto,
 
+                )
+        }
+    }
+}
 @Preview(
     showBackground = true,
     showSystemUi = true
 )
-
 @Composable
 fun GreetingPreview2() {
     Lab4Theme {
         Pantalla2(
-            R.drawable.outline_add_home_24,  R.drawable.imagen_5
-
+            R.drawable.outline_add_home_24,
+            R.drawable.imagen_5,
+            R.drawable.perfil,
 
         )
     }
 }
+
+
